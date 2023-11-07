ae659cfd24c7447519a0871831016588a6fe8365
Fabian_Avila <116823446+fabian-4vila@users.noreply.github.com>
2023-11-06 23:43:36 -0500

    first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const genreRouter = require('./genre.router');
+const directorRouter = require('./director.router');
+const actorRouter = require('./actor.router');
+const movieRouter = require('./movie.router');
 const router = express.Router();
 
 // colocar las rutas aquí
+router.use("/genres", genreRouter);
+router.use("/directors", directorRouter);
+router.use("/actors", actorRouter);
+router.use("/movies",movieRouter);
 
 
 module.exports = router;
